c22b88263f00725bac1a2cdc6e5de09b73ee26cf
dawardeepak <deepak.dawar@ndsu.edu>
2015-01-25 20:12:14 -0600

    next step

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,29 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
+
   describe "Home page" do
-    
-    it "should have the content 'Sample App'" do
-        visit '/static_pages/home'
-        page.should have_content('Sample App')
+
+    it "should have the h1 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text=>'Sample App')
     end
   end
-    
+
   describe "Help page" do
-    
-    it "should have the content 'Help'" do
+
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
-      end
-    end  
-  
+      page.should have_selector('h1', :text=>'Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text=>'About Us')
+    end
+  end
+
 end
